21a9648e1b74db1083bf5f189e2eef8719ae32be
Angelo Munhoz <angelomca09@gmail.com>
2025-02-12 19:12:34 -0300

    Installs fastify and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello, World!");
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import dotenv from "dotenv";
+
+// Load the environment variables
+dotenv.config();
+
+const app = fastify();
+
+// Enable CORS for all routes
+app.register(cors, {
+  origin: true,
+});
+
+//TODO: Add your routes here
+app.get("/", async () => {
+  return { hello: "world" };
+});
+
+// Get the host and port from the environment variables or use default values
+const host = process.env.BASE_URL || "localhost";
+
+// + is used to convert the string to a number
+const port = +(process.env.PORT || 3000);
+
+app.listen({ host, port })
+  .then((address) => {
+    console.log(`Server running on ${address} !`);
+  })
